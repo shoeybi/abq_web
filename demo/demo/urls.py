de79f3919d42244905121e035444d8b08d333ea6
Mohammad <shoeybim@gmail.com>
2013-01-27 13:18:29 -0800

    basic home page is working

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+# for direct use of template
+from django.views.generic.simple import direct_to_template
+# for admin tools
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,9 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'demo.views.home', name='home'),
     # url(r'^demo/', include('demo.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',direct_to_template,{'template': 'home.html'}),
+
 )
