912ef32c73b151e05fe40bc65c63855ef533733c
Mohammad <shoeybim@gmail.com>
2013-06-25 10:50:10 -0700

    login and logout are working

--- awd/awd/urls.py
+++ awd/awd/urls.py
@@ -15,6 +15,9 @@ urlpatterns = patterns(
     # base
     url(r'^base/$',TemplateView.as_view(template_name="base.html")),
 
+    # base
+    url(r'^profile/$',TemplateView.as_view(template_name="profile.html")),
+
     # home
     url(r'^home/$','abq.views.LoginRequest'),
 
