4142f84735804d1cd54e9125551d2159def1c178
mohammad <mohammad@abaqual.(none)>
2013-08-17 10:43:35 -0700

    demo was removed

--- demo/expert/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Expert(models.Model):
-    user = models.OneToOneField(User)
-    desktopname = ""
-    def __unicode__(self):
-        return self.user.username
-    
