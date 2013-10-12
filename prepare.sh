74343aa925586f02c0115129d55ae2c99b1e9e71
Mohammad <shoeybim@gmail.com>
2013-10-12 10:13:00 -0700

    latest

--- prepare.sh
+++ prepare.sh
@@ -1,16 +1,22 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install emacs -y
+
 sudo apt-get install gcc -y
 sudo apt-get install build-essential -y
 sudo apt-get install gfortran -y
-sudo apt-get install git-core -y
+
 sudo curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | sudo python
 rm -f setuptools-*.tar.gz
 sudo apt-get install python-pip -y
 sudo apt-get install python-dev -y
 sudo pip install --upgrade pip
 sudo pip install --upgrade setuptools
+
+sudo apt-get install emacs -y
+sudo apt-get install git-core -y
 git clone https://github.com/shoeybi/abq_web.git
+
 cd abq_web
 sudo pip install -r requirements.pip
