8d9ef812be134b3b51dd5ca822fe88d4b372e88c
Mohammad <shoeybim@gmail.com>
2014-01-22 15:45:39 -0800

    dialog box decorated

--- awd/static/js/launch_workspace.js
+++ awd/static/js/launch_workspace.js
@@ -1,8 +1,8 @@
 $(function() {
-    jQuery("#testb").dialog({
+    jQuery("#ws_launch_form").dialog({
 	autoOpen: false,
 	height: 300,
-	width: 350,
+	width: 300,
 	modal: true,
 	cache: false,
 	open: function() {
@@ -10,17 +10,17 @@ $(function() {
 		$.ajax({
 		    url: '/console/',
 		    type: 'post',
-		    data: $("#testb").serialize(),
+		    data: $("#ws_launch_form").serialize(),
 		    success: function(data) {
-			$("#testb").load($(data).find("#testb"));
+			$("#ws_launch_form").load($(data).find("#ws_launch_form"));
 			$("#id_os").replaceWith($(data).find("#id_os"));
 		    }
 		});
 	    });
 	},
     });
-  jQuery("#butt").click(function() {
-      jQuery("#testb").dialog("open");
+  jQuery("#ws_launch_butt").click(function() {
+      jQuery("#ws_launch_form").dialog("open");
   });
   
   
